17c2176e50618d0ed9eaeb52adcb14decfe9e023
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-03-13 13:34:15 +0700

    Menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ 05_Generics/Jurnal_05/Jurnal_05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T a, T b, T c) where T : struct
+    {
+        dynamic x = a;
+        dynamic y = b;
+        dynamic z = c;
+        return x + y + z;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        double hasil = Penjumlahan.JumlahTigaAngka(22.0, 11.0, 10.0);
+        Console.WriteLine($"Hasil penjumlahan: {hasil}");
+    }
+}
