5fa7a759acea884655c519e42afe6685d317d93b
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-03-13 11:06:08 +0700

    menambahkan class DataGeneric

---
+++ 05_Generics/TP_05_2211104024/TP_05_2211104024/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("2211104024");
+        data.PrintData();
+    }
+}
