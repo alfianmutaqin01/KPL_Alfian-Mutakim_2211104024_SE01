2d2161835bd98687612f723d39b48ceab8cfcc0a
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-03-13 13:21:55 +0700

    05

--- 05_Generics/TP_05_2211104024/TP_05_2211104024/Program.cs
+++ 05_Generics/TP_05_2211104024/TP_05_2211104024/Program.cs
@@ -1,23 +1,21 @@
-using System;
-
-class DataGeneric<T>
-{
-    private T data;
-    public DataGeneric(T data)
-    {
-        this.data = data;
-    }
-    public void PrintData()
-    {
-        Console.WriteLine($"Data yang tersimpan adalah: {data}");
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        DataGeneric<string> data = new DataGeneric<string>("2211104024");
-        data.PrintData();
-    }
-}
+﻿class DataGeneric<T>
+ {
+     private T data;
+     public DataGeneric(T data)
+     {
+         this.data = data;
+     }
+     public void PrintData()
+     {
+         Console.WriteLine($"Data yang tersimpan adalah: {data}");
+     }
+ }
+ 
+ class Program
+ {
+     static void Main(string[] args)
+     {
+         DataGeneric<string> data = new DataGeneric<string>("2211104005");
+         data.PrintData();
+     }
+ }
