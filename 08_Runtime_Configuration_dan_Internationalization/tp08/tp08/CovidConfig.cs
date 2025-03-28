cf67a9fc1274427dd1f07778e7eb1026c64be264
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-03-28 21:58:25 +0700

    Menambahkan file proyek dan konfigurasi untuk pengolahan input berbasis tata bahasa

---
+++ 08_Runtime_Configuration_dan_Internationalization/tp08/tp08/CovidConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+public class CovidConfig
+{
+    private const string configFile = "covid_config.json";
+
+    public string SatuanSuhu { get; set; }
+    public int BatasHariDemam { get; set; }
+    public string PesanDitolak { get; set; }
+    public string PesanDiterima { get; set; }
+
+    public CovidConfig()
+    {
+        if (File.Exists(configFile))
+        {
+            string json = File.ReadAllText(configFile);
+            var configData = JsonConvert.DeserializeObject<CovidConfig>(json);
+            SatuanSuhu = configData.SatuanSuhu;
+            BatasHariDemam = configData.BatasHariDemam;
+            PesanDitolak = configData.PesanDitolak;
+            PesanDiterima = configData.PesanDiterima;
+        }
+        else
+        {
+            SatuanSuhu = "celcius";
+            BatasHariDemam = 14;
+            PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+        }
+    }
+
+    public void UbahSatuan()
+    {
+        SatuanSuhu = SatuanSuhu == "celcius" ? "fahrenheit" : "celcius";
+        SimpanKonfigurasi();
+    }
+
+    private void SimpanKonfigurasi()
+    {
+        var json = JsonConvert.SerializeObject(this, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(configFile, json);
+    }
+}
---
+++ 08_Runtime_Configuration_dan_Internationalization/tp08/tp08/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        CovidConfig config = new CovidConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        if ((config.SatuanSuhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+            (config.SatuanSuhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5))
+        {
+            if (hariDemam < config.BatasHariDemam)
+                Console.WriteLine(config.PesanDiterima);
+            else
+                Console.WriteLine(config.PesanDitolak);
+        }
+        else
+        {
+            Console.WriteLine(config.PesanDitolak);
+        }
+
+        Console.WriteLine("Apakah ingin mengubah satuan suhu? (y/n)");
+        if (Console.ReadLine().ToLower() == "y")
+        {
+            config.UbahSatuan();
+            Console.WriteLine("Satuan suhu berhasil diubah ke " + config.SatuanSuhu);
+        }
+    }
+}
