10f0549105ea0eedf4bdcfa12fad8c21417cc4ce
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-04-19 21:57:09 +0700

    Add static web assets and NuGet configuration for TP_9_swager_2211104024 project
    - Created static web assets references and removed files.
    - Added MSBuild properties files for static web assets.
    - Generated NuGet dependency specification and properties files.
    - Included project.assets.json for managing package dependencies.
    - Established project cache for NuGet package management.

---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/MahasiswaController.cs
@@ -0,0 +1,60 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace tpmodul9_2211104024.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        public class Mahasiswa
+        {
+            public string Nama { get; set; }
+            public string Nim { get; set; }
+        }
+
+        private static List<Mahasiswa> listMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Alfian Mutakim", Nim = "2211104017" },
+            new Mahasiswa { Nama = "Nadia Putri Rahmaniar", Nim = "2211104012" },
+            new Mahasiswa { Nama = "Rafli Dhafin Kamil", Nim = "2211104023" },
+            new Mahasiswa { Nama = "Nita Fitrotul Mar'ah", Nim = "221110405" },
+            new Mahasiswa { Nama = "Muhammad Edgar Nadhif", Nim = "2211104020" },
+            new Mahasiswa { Nama = "Muhammad Dhimas Afrizal", Nim = "2211104025" },
+        };
+
+        // GET /api/mahasiswa
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMahasiswa;
+        }
+
+        // GET /api/mahasiswa/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> Get(int id)
+        {
+            if (id < 0 || id >= listMahasiswa.Count)
+                return NotFound();
+            return listMahasiswa[id];
+        }
+
+        // POST /api/mahasiswa
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mhs)
+        {
+            listMahasiswa.Add(mhs);
+        }
+
+        // DELETE /api/mahasiswa/{id}
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id >= 0 && id < listMahasiswa.Count)
+            {
+                listMahasiswa.RemoveAt(id);
+            }
+        }
+    }
+}
